291dbef587ecfcb064607dc8f77c8d264970e5c5
lmm214 <fivemu@gmail.com>
2022-02-01 11:02:18 +0800

    yq

--- themes/hello-friend/static/fcircle-public.js
+++ themes/hello-friend/static/fcircle-public.js
@@ -6,7 +6,7 @@ Last Modified time : 20220130 19:14 by https://immmmm.com
 //默认数据
 var fdata = {
   apiurl: 'https://circle-of-friends-simple.vercel.app/',
-  initnumber: 100,  //首次加载文章数
+  initnumber: 20,  //首次加载文章数
   stepnumber: 10,  //更多加载文章数
   article_sort: 'updated', //文章排序 updated or created
   error_img: 'https://sdn.geekzu.org/avatar/57d8260dfb55501c37dde588e7c3852c'
