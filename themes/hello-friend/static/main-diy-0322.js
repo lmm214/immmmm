4397a83d499fbccf0817c913f3cd348640ff1137
lmm214 <fivemu@gmail.com>
2022-03-22 21:52:50 +0800

    movies

--- themes/hello-friend/static/main-diy-0322.js
+++ themes/hello-friend/static/main-diy-0322.js
@@ -70,66 +70,6 @@ jQuery(document).ready(function ($) {
     var replace_href = this_href.replace(parse_url, '$3');
     if (this_href != replace_href && location_href != replace_href) { $(this).attr('target', '_blank'); }
   });
-  //豆瓣图书电影条目
-  $(".post-content a[href*='douban.com/subject/']").each(function () {
-    var _this = $(this);
-    var str = _this.attr("href");
-    var db_reg = /^https\:\/\/(movie|book)\.douban\.com\/subject\/([0-9]+)\/?/;
-    if (db_reg.test(str)) {
-      var db_type = str.replace(db_reg, "$1");
-      var db_id = str.replace(db_reg, "$2").toString();
-      var db_api = "https://api.weajs.com/v1/";
-      if (db_type == 'movie') {
-        var ls_item = 'movie' + db_id;
-        var url = db_api + "movie/" + db_id + "/";
-        if (localStorage.getItem(ls_item) == null || localStorage.getItem(ls_item) == 'undefined') {
-          $.ajax({
-            url: url,
-            type: 'GET',
-            dataType: "json",
-            success: function (data) {
-              localStorage.setItem(ls_item, JSON.stringify(data));
-              moiveShow(_this, ls_item)
-            }
-          });
-        } else {
-          moiveShow(_this, ls_item)
-        }
-      } else if (db_type == 'book') {
-        var ls_item = 'book' + db_id;
-        var url = db_api + "book/" + db_id;
-        if (localStorage.getItem(ls_item) == null || localStorage.getItem(ls_item) == 'undefined') {
-          $.ajax({
-            url: url,
-            type: 'GET',
-            dataType: 'json',
-            success: function (data) {
-              localStorage.setItem('book' + db_id, JSON.stringify(data));
-              bookShow(_this, ls_item)
-            }
-          });
-        } else {
-          bookShow(_this, ls_item)
-        }
-      }
-    }
-  });
-  function moiveShow(_this, ls_item) {
-    var storage = localStorage.getItem(ls_item);
-    var data = JSON.parse(storage);
-    var str = _this.attr("href");
-    //console.log(data)
-    var db_star = Math.ceil(data.rating);
-    $("<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' href='" + str + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>导演：" + data.directors + " / 类型：" + data.genres + " / " + data.pubdate + "</time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.intro + "</section></div></div><img loading='lazy' class='post-preview--image' src=" + data.cover + "></div>").replaceAll(_this);
-  }
-  function bookShow(_this, ls_item) {
-    var storage = localStorage.getItem(ls_item);
-    var data = JSON.parse(storage);
-    var str = _this.attr("href");
-    ///console.log(data)
-    var db_star = Math.ceil(data.rating);
-    $("<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' href='" + str + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>作者：" + data.author + " </time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.intro + "</section></div></div><img loading='lazy' class='post-preview--image' src=" + data.cover + "></div>").replaceAll(_this);
-  }
 });
 
 $(document).ready(function(){
