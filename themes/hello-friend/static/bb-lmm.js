21d40b19a629b1d00b038afef1175f0e256987fd
lmm214 <fivemu@gmail.com>
2022-10-07 13:40:34 +0800

    bb

--- themes/hello-friend/static/bb-lmm.js
+++ themes/hello-friend/static/bb-lmm.js
@@ -80,8 +80,7 @@ function updateHTMl(data){
   const INLINE_CODE_REG = /`([\S ]+?)`/g;
   const PLAIN_TEXT_REG = /([\S ]+)/g
   for(var i=0;i < data.length;i++){
-      var bbTime = '<p class="datatime">'+new Date(data[i].createdTs * 1000).toLocaleString()+'</p>'
-      //console.log(data[i].content)
+      console.log(data[i].content)
       var bbContREG = data[i].content
         .replace(CODE_BLOCK_REG, "<pre lang='$1'>\n$2</pre>$3")
         .replace(TODO_LIST_REG, "<p><span class='todo-block todo' data-value='TODO'></span>$1</p>$2")
@@ -98,11 +97,10 @@ function updateHTMl(data){
         .replace(PLAIN_LINK_REG, "<a class='link' target='_blank' rel='noreferrer' href='$1'>$1</a> ")
         .replace(TAG_REG, "<span class='tag-span'>#$1</span> ")
         .replace(PLAIN_TEXT_REG, "$1")
-        //console.log(bbContREG)
+        console.log(bbContREG)
       //解析内置资源文件
       if(data[i].resourceList && data[i].resourceList.length > 0){
         var resourceList = data[i].resourceList;
-        //console.log(resourceList)
         var imgUrl='',resUrl='';
         for(var j=0;j < resourceList.length;j++){
           var restype = resourceList[j].type.slice(0,5)
@@ -116,11 +114,10 @@ function updateHTMl(data){
         bbContREG += imgUrl
         bbContREG += resUrl
       }
-      var bbCont = '<p class="datacont">'+bbContREG+'</p>'
-      result += '<li class="item"><div>'+bbTime+bbCont+'</div></li>'
+      result += "<li class='item'><div class='itemdiv'><p class='datatime'>"+new Date(data[i].createdTs * 1000).toLocaleString()+"</p><div class='datacont'>"+bbContREG+"</div></div></li>"
   }// end for
-  var bbBefore = '<section class="timeline"><ul><div class="list">'
-  var bbAfter = '</div></ul></section>'
+  var bbBefore = "<section class='timeline'><ul class='list'>"
+  var bbAfter = "</ul></section>"
   resultAll = bbBefore + result + bbAfter
   bbDom.insertAdjacentHTML('beforeend', resultAll);
   document.querySelector('button.button-load').textContent= '加载更多';
