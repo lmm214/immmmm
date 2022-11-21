32f51caeea60222590ed23ecb8e40eaf3d8d121d
lmm214 <fivemu@gmail.com>
2022-11-21 20:47:43 +0800

    withtimeout

--- themes/hello-friend/static/bbs-lmm.js
+++ themes/hello-friend/static/bbs-lmm.js
@@ -138,8 +138,8 @@ const withTimeout = (millis, promise) => {
 const fetchBBser = async () => {
   const results = await Promise.allSettled(urls.map(
     //限时
-    //url => withTimeout(2000,fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data))
-    url => fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data)
+    url => withTimeout(2000,fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data))
+    //url => fetch(url.host+"api/memo?creatorId="+url.creatorId+"&rowStatus=NORMAL&limit="+limit).then(response => response.json()).then(resdata => resdata.data)
   )).then(results=> {
     //console.log(results)
     bbDom.innerHTML = ''
