f86d6337d81b14499ed352e1f3f202bac15825a2
lmm214 <fivemu@gmail.com>
2023-01-07 13:17:31 +0800

    bbs

--- themes/hello-friend/static/bbs-lmm.js
+++ themes/hello-friend/static/bbs-lmm.js
@@ -2,28 +2,29 @@
 Last Modified time : 20221120 21:32 by https://immmmm.com
 */
 const urls = [
-  {host:"https://me.edui.fun/",creatorId:"101",md5:"ba83fa02fc4b2ba621514941307e21be"},
-  {host:"https://bb.elizen.me/",creatorId:"101",md5:"f65df4d87240feb1cb247857a621a48f"},
-  {host:"https://memos.eallion.com/",creatorId:"101",md5:"171e4c30959e8c077a6c58b958624b31"},
-  {host:"https://qzone.boyhu.cn/",creatorId:"101",md5:"55be217893c75baf8571837197de4a3e"},
-  {host:"https://me.chenplus.com/",creatorId:"101",md5:"d1ed9c15ba6d0cb18464118c6288a8ff"},
-  {host:"https://memos.life97.top/",creatorId:"101",md5:"d41d8cd98f00b204e9800998ecf8427e"},
-  {host:"https://memos.1900.live/",creatorId:"101",md5:"cc38267b10cc25dfc62209f8ca34589e"},
-  {host:"https://bb.seersu.top/",creatorId:"101",md5:"84b712148a63b44dd97ede997bc3efa5"},
+  {host:"https://me.edui.fun/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/ba83fa02fc4b2ba621514941307e21be"},
+  {host:"https://bb.elizen.me/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/f65df4d87240feb1cb247857a621a48f"},
+  {host:"https://memos.eallion.com/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/171e4c30959e8c077a6c58b958624b31"},
+  {host:"https://qzone.boyhu.cn/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/55be217893c75baf8571837197de4a3e"},
+  {host:"https://me.chenplus.com/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/d1ed9c15ba6d0cb18464118c6288a8ff"},
+  {host:"https://memos.life97.top/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/d41d8cd98f00b204e9800998ecf8427e"},
+  {host:"https://memos.1900.live/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/cc38267b10cc25dfc62209f8ca34589e"},
+  {host:"https://bb.seersu.top/",creatorId:"101",imgsrc:"https://cravatar.cn/avatar/84b712148a63b44dd97ede997bc3efa5"},
+  {host:"https://memos.icebear.tech/",creatorId:"1",imgsrc:"https://www.domon.cn/content/images/size/w150/2020/06/xavatar_shiba-3.jpg.pagespeed.ic.3PcXeGZpNB.webp"},
 ]
 
 var bbDom = document.querySelector('#bbs');
 var load = '<div id="load" onclick="nextFetch()" ><button class="load-btn button-load">加载更多</button></div>'
 var loading = '<div class="loader"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/></svg></div>'
 var bbsDatas = [],bbsData = {},nextDatas = [],nextData = {},limit = 2
-var page = 1,offset = 0,nextLength = 0,nextDom='',bbUrlNow = '',md5Now = '',hostNow = '',creIdNow = ''
+var page = 1,offset = 0,nextLength = 0,nextDom='',bbUrlNow = '',imgsrcNow = '',hostNow = '',creIdNow = ''
 
 bbDom.innerHTML = loading
 allUrls()
 function allUrls(){
   var myHtml = ''
   for(var i=0;i < urls.length;i++){
-    myHtml += '<div class="bbs-urls " onclick="urlsNow(this)" data-host="'+urls[i].host+'" data-creatorId="'+urls[i].creatorId+'" data-md5="'+urls[i].md5+'" data-index="'+i+'"><img src="https://cravatar.cn/avatar/'+urls[i].md5+'" alt=""></div>'
+    myHtml += '<div class="bbs-urls " onclick="urlsNow(this)" data-host="'+urls[i].host+'" data-creatorId="'+urls[i].creatorId+'" data-imgsrc="'+urls[i].imgsrc+'" data-index="'+i+'"><img src="'+urls[i].imgsrc+'" alt=""></div>'
   }
   myHtml += '<div class="bbs-urls urls-button" onclick="urlsNow(this)" data-type="random"><svg t="1665928089691" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2562" width="32" height="32"><path d="M913.2 672l98.8 57.1c5.3 3.1 5.3 10.8 0 13.9l-43.4 25L710.4 924c-2.7 1.5-6-0.4-6-3.5V772c0-2.2-1.8-4-4-4H544c-70.4 0-134.4-28.8-180.8-75.2-11.1-11.1-21.2-23.2-30.1-36.1-6.4-9.2-20-9.1-26.4 0.1C260.5 723.9 183.1 768 96 768h-48c-26.5 0-48-21.5-48-48s21.5-48 48-48h48c42.5 0 82.6-16.7 112.9-47.1 30.4-30.4 47.1-70.5 47.1-112.9s-16.7-82.6-47.1-112.9C178.6 368.7 138.4 352 96 352h-48c-26.5 0-48-21.5-48-48s21.5-48 48-48h48c70.4 0 134.4 28.8 180.8 75.2 11.1 11.1 21.2 23.2 30.1 36.1 6.4 9.2 20 9.1 26.4-0.1 46.3-67 123.6-111.1 210.8-111.1H700.4c2.2 0 4-1.8 4-4V103.4c0-3.1 3.3-5 6-3.5l258.2 156 43.4 25.1c5.3 3.1 5.3 10.8 0 13.9L913.2 352 710.4 476c-2.7 1.5-6-0.4-6-3.5V356c0-2.2-1.8-4-4-4H544c-42.5 0-82.6 16.7-112.9 47.1-30.4 30.4-47.1 70.5-47.1 112.9 0 42.5 16.7 82.6 47.1 112.9C461.4 655.3 501.5 672 544 672H700.4c2.2 0 4-1.8 4-4V551.4c0-3.1 3.3-5 6-3.5L913.2 672z" p-id="2563" fill="#f5f5f5"></path></svg></div>'
   myHtml += '<div class="bbs-urls urls-button"><a href="https://immmmm.com/bbs-by-memos/"><svg t="1665929410343" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6308" width="32" height="32"><path d="M906.212134 565.732986 565.732986 565.732986 565.732986 906.212134C565.732986 926.013685 541.666486 959.972 511.97312 959.972 482.297674 959.972 458.213254 926.013685 458.213254 906.212134L458.213254 565.732986 117.734106 565.732986C97.950475 565.732986 63.97424 541.666486 63.97424 511.97312 63.97424 482.279754 97.950475 458.213254 117.734106 458.213254L458.213254 458.213254 458.213254 117.734106C458.213254 97.950475 482.297674 63.97424 511.97312 63.97424 541.666486 63.97424 565.732986 97.950475 565.732986 117.734106L565.732986 458.213254 906.212134 458.213254C925.995765 458.213254 959.972 482.279754 959.972 511.97312 959.972 541.666486 925.995765 565.732986 906.212134 565.732986Z" p-id="6309" fill="#f5f5f5"></path></svg></a></div>'
@@ -57,13 +58,13 @@ function urlsNow(e){
       var num = Math.round(Math.random() * (urls.length-1))
       hostNow = urls[num].host
       creIdNow = urls[num].creatorId
-      md5Now =urls[num].md5
+      imgsrcNow =urls[num].imgsrc
       domUrls[num].classList.add("url-now")
     }else{
       domUrls[e.getAttribute("data-index")].classList.add("url-now")
       hostNow = e.getAttribute("data-host")
       creIdNow = e.getAttribute("data-creatorId")
-      md5Now = e.getAttribute("data-md5")
+      imgsrcNow = e.getAttribute("data-imgsrc")
     }
     bbUrlNow = hostNow+"api/memo?creatorId="+creIdNow+"&rowStatus=NORMAL&limit=10"
     fetch(bbUrlNow).then(res => res.json()).then( resdata =>{
@@ -76,7 +77,7 @@ function urlsNow(e){
               updatedTs: resValue.updatedTs,
               creatorId:resValue.creatorId,
               creator: resValue.creator.nickname || resValue.creator.name,
-              mailmd5: md5Now,
+              imgsrc: imgsrcNow,
               content: resValue.content,
               resourceList: resValue.resourceList,
               url:hostNow
@@ -117,7 +118,7 @@ function getNextList(){
         updatedTs: resValue.updatedTs,
         creatorId:resValue.creatorId,
         creator: resValue.creator.nickname || resValue.creator.name,
-        mailmd5: md5Now,
+        imgsrc: imgsrcNow,
         content: resValue.content,
         resourceList: resValue.resourceList,
         url:hostNow
@@ -153,7 +154,7 @@ const fetchBBser = async () => {
             updatedTs: resValue.updatedTs,
             creatorId:resValue.creatorId,
             creator: resValue.creator.nickname || resValue.creator.name,
-            mailmd5: urls[i].md5,
+            imgsrc: urls[i].imgsrc,
             content: resValue.content,
             resourceList: resValue.resourceList,
             url:urls[i].host
@@ -235,7 +236,7 @@ function updateHTMl(data){
           bbContREG += '<div class="resour">'+resUrl+'</div>'
         }
       }
-      result += '<li class=""><div class="bbs-avatar"><img src="https://cravatar.cn/avatar/'+data[i].mailmd5+'" alt=""><a href="'+data[i].url+'u/'+data[i].creatorId+'" target="_blank" rel="noopener noreferrer" class="bbs-creator">'+data[i].creator+'</a><span class="bbs-dot">·</span><span class="bbs-date">'+new Date(data[i].updatedTs * 1000).toLocaleString()+'</span></div><div class="bbs-content"><div class="bbs-text">'+bbContREG+'</div></div></li>'
+      result += '<li class=""><div class="bbs-avatar"><img src="'+data[i].imgsrc+'" alt=""><a href="'+data[i].url+'u/'+data[i].creatorId+'" target="_blank" rel="noopener noreferrer" class="bbs-creator">'+data[i].creator+'</a><span class="bbs-dot">·</span><span class="bbs-date">'+new Date(data[i].updatedTs * 1000).toLocaleString()+'</span></div><div class="bbs-content"><div class="bbs-text">'+bbContREG+'</div></div></li>'
   }// end for
   var bbBefore = "<section class='bbs-timeline'><ul class='list'>"
   var bbAfter = "</ul></section>"
