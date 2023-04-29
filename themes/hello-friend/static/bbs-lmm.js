ecaae65e29568c8b8594696f87d190b7e74b3240
lmm214 <fivemu@gmail.com>
2023-04-29 15:20:53 +0800

    bbs

--- themes/hello-friend/static/bbs-lmm.js
+++ themes/hello-friend/static/bbs-lmm.js
@@ -45,7 +45,8 @@ const urls = [
   {home:"https://blog.roccoshi.top/",host:"https://memos.roccoshi.top/",creatorId:"1",comment:'',imgsrc:"https://youpai.roccoshi.top/img/avatar.jpg"},
   {home:"https://l22.org/",host:"https://note.l22.org/",creatorId:"1",comment:'',imgsrc:"https://cravatar.cn/avatar/4b0d33a08ac73dc07a5293f14232ca53"},
   {home:"https://yunlogs.com/",host:"https://note.zdm.im/",creatorId:"1",comment:'',imgsrc:"https://cravatar.cn/avatar/d41d8cd98f00b204e9800998ecf8427e"},
-  {home:"https://dongjunke.cn/",host:"https://memos.dongjunke.cn/",creatorId:"1",comment:'',imgsrc:"https://cravatar.cn/avatar/42542189ddb33064599dc88a5149bea7"}
+  {home:"https://dongjunke.cn/",host:"https://memos.dongjunke.cn/",creatorId:"1",comment:'',imgsrc:"https://cravatar.cn/avatar/42542189ddb33064599dc88a5149bea7"},
+  {home:"https://emmm.space/",host:"https://memos.emmm.space/",creatorId:"1",comment:'',imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=LxEGgNG5gRmKSbHibgaEFUw&kti=ZEy2AAAAAAE&s=140&t=1655774580"}
 ]
 
 var bbDom = document.querySelector('#bbs');
