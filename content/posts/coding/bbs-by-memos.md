3867dede779f5901bbd314ab8fbec201f013ad24
lmm214 <fivemu@gmail.com>
2023-06-26 22:38:59 +0800

    bbs

--- content/posts/coding/bbs-by-memos.md
+++ content/posts/coding/bbs-by-memos.md
@@ -1,28 +1,21 @@
 ---
-title: "哔哔广场，欢迎围观"
-date: 2022-10-11T22:56:57+0800
+title: "哔哔广场 x Twikoo"
+date: 2023-06-11T22:26:57+0800
 tags: [折腾]
-feature: https://r2.immmmm.com/2022/10/bbs-1.png
+feature: https://r2.immmmm.com/2023/06/twikoo-x-bbs.png
 ---
 
 围观链接： <https://immmmm.com/bbs/>
 
-免去一个个 Memos 网址点开，直接 Fetch 汇总最新 memo 并排序渲染。 
+2023-06-26 更新：既然折腾了 [《Memos x Twikoo》](https://immmmm.com/memos-with-twikoo/)，何不再来个「哔哔广场」版，安排！
 
 <!--more-->
 
-当前 Fetch 了以下站点的 Memos，欢迎丢站点链接，立马加进去～
+当面，未启用 Twikoo 也欢迎丢 Memos 站点链接~
 
-```javascript
-const urls = [
-  {host:"https://me.edui.fun/",creatorId:"101",md5:"ba83fa02fc4b2ba621514941307e21be"},
-  {host:"https://me.edui.fun/",creatorId:"102",md5:"8faeaa1b58a25c03be347b5a7fb5b42a"},
-  {host:"https://bb.elizen.me/",creatorId:"101",md5:"f65df4d87240feb1cb247857a621a48f"},
-  {host:"https://memos.eallion.com/",creatorId:"101",md5:"171e4c30959e8c077a6c58b958624b31"},
-  {host:"https://qzone.boyhu.cn/",creatorId:"101",md5:"55be217893c75baf8571837197de4a3e"},
-  {host:"https://me.chenplus.com/",creatorId:"101",md5:"d1ed9c15ba6d0cb18464118c6288a8ff"},
-  {host:"https://memos.life97.top/",creatorId:"101",md5:"d41d8cd98f00b204e9800998ecf8427e"},
-  {host:"https://memos.1900.live/",creatorId:"101",md5:"cc38267b10cc25dfc62209f8ca34589e"},
-  {host:"https://memos.m1s.one/",creatorId:"101",md5:"c0a3e4fce8d8ae0651fd0b1ed717d186"},
-]
 ```
+host:"https://me.edui.fun/",
+twiEnv:'https://tk.edui.fun/',
+```
+
+会第一时间加入到「哔哔广场」和电报[「Memos 广场」](https://t.me/memos_bbs)。
--- themes/hello-friend/static/bbs-lmm.js
+++ themes/hello-friend/static/bbs-lmm.js
@@ -329,7 +329,7 @@ function updateHTMl(data){
       var comSVG = '<span class="bbs-coment-svg"><svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="20" height="24"><path d="M816 808H672c-4.8 0-8 1.6-11.2 4.8l-80 80c-36.8 36.8-97.6 36.8-136 0l-80-80c-3.2-3.2-6.4-4.8-11.2-4.8h-144c-70.4 0-128-57.6-128-128V232c0-70.4 57.6-128 128-128h608c70.4 0 128 57.6 128 128v448C944 750.4 886.4 808 816 808zm0-64c35.2 0 64-28.8 64-64V232c0-35.2-28.8-64-64-64H208c-35.2 0-64 28.8-64 64v448c0 35.2 28.8 64 64 64h144c20.8 0 41.6 8 56 24l80 80c12.8 12.8 32 12.8 44.8 0l80-80c14.4-14.4 35.2-24 56-24H816zM320 408c27.2 0 48 20.8 48 48v32c0 27.2-20.8 48-48 48s-48-20.8-48-48v-32c0-27.2 20.8-48 48-48zm192 0c27.2 0 48 20.8 48 48v32c0 27.2-20.8 48-48 48s-48-20.8-48-48v-32c0-27.2 20.8-48 48-48zm192 0c27.2 0 48 20.8 48 48v32c0 27.2-20.8 48-48 48s-48-20.8-48-48v-32c0-27.2 20.8-48 48-48z" fill="#cccccc"/></svg></span>'
 
       if(comment == '1'){
-        if(twiEnv){
+        if(twiEnv && twiEnv != 'undefined'){
           result += '<a data-id="'+memoId+'" data-env="'+twiEnv+'" data-path="'+memoUrl+'" onclick="loadTwikoo(this)" href="javascript:void(0)" rel="noopener noreferrer">'+comSVG+'</a></div><div class="bbs-content"><div class="bbs-text">'+bbContREG+'</div><div class="item-twikoo twikoo-'+memoId+' d-none"><div id="twikoo-'+memoId+'"></div></div></div></li>'
         }else{
           result += '<a href="'+memoUrl+'" target="_blank" rel="noopener noreferrer">'+comSVG+'</a></div><div class="bbs-content"><div class="bbs-text">'+bbContREG+'</div></div></li>'
