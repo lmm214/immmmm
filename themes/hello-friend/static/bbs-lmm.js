e204e96303ace2caae9dfa77006a5b293f56a54a
lmm214 <fivemu@gmail.ocm>
2023-08-09 12:25:45 +0800

    bbs

--- themes/hello-friend/static/bbs-lmm.js
+++ themes/hello-friend/static/bbs-lmm.js
@@ -32,23 +32,23 @@ const urls = [
   {home:"https://laozhang.org/",host:"https://memos.laozhang.org/",apiV1:'v1/',creatorId:"1",comment:'1',artEnv: 'https://artalk.laozhang.org/',artSite: 'memos',imgsrc:cdnGravatar+"679666f7bd1af3e55f0e51dd70ed161c"},
   {home:"https://veryjack.com/",host:"https://say.veryjack.com/",apiV1:'v1/',creatorId:"1",comment:'1',artEnv: 'https://artalk.veryjack.com',artSite: 'Jack\'s Space',imgsrc:cdnGravatar+"7a41a0e8e1df8e964fa1268193b03508"},
   {home:"https://usj.cc/",host:"https://memos.usj.cc/",apiV1:'',creatorId:"1",comment:'1',artEnv:'https://artalk.usj.cc',artSite:'优世界',imgsrc:cdnGravatar+"1cce0a22c2c7648eab76ec876c6a54d9"},
-  {home:"https://blog.roccoshi.top/",host:"https://memos.roccoshi.top/",apiV1:'v1/',creatorId:"1",comment:'1',imgsrc:"https://youpai.roccoshi.top/img/avatar.jpg"},
-  {home:"https://www.cyuanx.icu/",host:"https://mome.cyuanx.icu/",apiV1:'',creatorId:"1",comment:'',imgsrc:"https://www.cyuanx.icu/wp-content/uploads/2023/03/cropped-07b4b32029661fd9ce1200ac9b937f1.jpg"},
-  {home:"https://xsinger.me/",host:"https://isay.live/",apiV1:'v1/',creatorId:"101",comment:'',imgsrc:cdnGravatar+"f0e33f4d097fe2e9fd74b9b129e7a655"},
-  {home:"https://noionion.top/",host:"https://memos.noionion.cn/",apiV1:'',creatorId:"1",comment:'',imgsrc:cdnGravatar+"d240a727ea154be465796ef2644aac16"},
-  {home:"https://www.wananaiko.com/",host:"https://memo.wananaiko.com/",apiV1:'',creatorId:"1",comment:'',imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=7AFd0gic7wVsNg2ToQrQU2w&s=140&t=1583250600"},
-  {home:"https://dusays.com/",host:"https://s.dusays.com/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:cdnGravatar+"28b57baa4e8f13fe4292ccb2de267e30"},
-  {home:"https://dusays.com/",host:"https://s.dusays.com/",apiV1:'v1/',creatorId:"9",comment:'',imgsrc:cdnGravatar+"c1b204bab687a23c8b6d7c8de11c7c59"},
-  {home:"https://www.4op.top/",host:"https://flomo.010316.xyz/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:cdnGravatar+"81f9378195d84e8ff73aad9ded2b9808"},
-  {home:"https://wiki-power.com/",host:"https://memos.wiki-power.com/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:cdnGravatar+"44218020ee434de43516c19f05c71593"},
-  {home:"https://blog.leonus.cn/",host:"https://m.leonus.cn/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=cZKBhtxe2iaxjSfbVYiaFgoQ&kti=Y9x6QwAAAAI&s=140&t=1672836908"},
-  {home:"https://eirms.com/",host:"https://memo.eirms.com/",apiV1:'',creatorId:"1",comment:'',imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=6bLfAytyUI7daRuxat0XxA&kti=ZDt5CwAAAAA&s=140&t=1646284093"},
-  {home:"https://cmsblog.cn/",host:"https://memos.cmsblog.cn/",apiV1:'',creatorId:"101",comment:'',imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=fcgI8ibPhO3zz3IvgL4bl7Q&kti=ZDuqEgAAAAI&s=140&t=1556624886"},
-  {home:"https://pipuwong.com/",host:"https://memos.pipuwong.com/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=5F8TcjFWpLMzia5ibn26vs5A&kti=ZFVJhgAAAAA&s=140&t=1679212034"},
-  {home:"https://ysicing.cloud/",host:"https://note.ysicing.cloud/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:cdnGravatar+"5ca16ee587d1e4befd805cf710361f26"},
-  {home:"https://www.kingname.info/",host:"https://memos.kingname.info/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:cdnGravatar+"2463f1bd85aea23703f320e67d33f349"},
-  {home:"https://idh.ink/",host:"https://idh.ink/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=NAuictuJAIjdPiaHlY6xbDWA&kti=ZHUWoAAAAAA&s=140&t=1683481629"},
-  {home:"https://leonfong.me/",host:"https://memo.leonfong.me/",apiV1:'v1/',creatorId:"1",comment:'',imgsrc:cdnGravatar+"b147bfa4888f1233882ba5b39ef3bffc"}
+  {home:"https://blog.roccoshi.top/",host:"https://memos.roccoshi.top/",apiV1:'v1/',creatorId:"1",imgsrc:"https://youpai.roccoshi.top/img/avatar.jpg"},
+  {home:"https://www.cyuanx.icu/",host:"https://mome.cyuanx.icu/",apiV1:'',creatorId:"1",imgsrc:"https://www.cyuanx.icu/wp-content/uploads/2023/03/cropped-07b4b32029661fd9ce1200ac9b937f1.jpg"},
+  {home:"https://xsinger.me/",host:"https://isay.live/",apiV1:'v1/',creatorId:"101",imgsrc:cdnGravatar+"f0e33f4d097fe2e9fd74b9b129e7a655"},
+  {home:"https://noionion.top/",host:"https://memos.noionion.cn/",apiV1:'',creatorId:"1",imgsrc:cdnGravatar+"d240a727ea154be465796ef2644aac16"},
+  {home:"https://www.wananaiko.com/",host:"https://memo.wananaiko.com/",apiV1:'',creatorId:"1",imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=7AFd0gic7wVsNg2ToQrQU2w&s=140&t=1583250600"},
+  {home:"https://dusays.com/",host:"https://s.dusays.com/",apiV1:'v1/',creatorId:"1",imgsrc:cdnGravatar+"28b57baa4e8f13fe4292ccb2de267e30"},
+  {home:"https://dusays.com/",host:"https://s.dusays.com/",apiV1:'v1/',creatorId:"9",imgsrc:cdnGravatar+"c1b204bab687a23c8b6d7c8de11c7c59"},
+  {home:"https://www.4op.top/",host:"https://flomo.010316.xyz/",apiV1:'v1/',creatorId:"1",imgsrc:cdnGravatar+"81f9378195d84e8ff73aad9ded2b9808"},
+  {home:"https://wiki-power.com/",host:"https://memos.wiki-power.com/",apiV1:'v1/',creatorId:"1",imgsrc:cdnGravatar+"44218020ee434de43516c19f05c71593"},
+  {home:"https://blog.leonus.cn/",host:"https://m.leonus.cn/",apiV1:'v1/',creatorId:"1",imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=cZKBhtxe2iaxjSfbVYiaFgoQ&kti=Y9x6QwAAAAI&s=140&t=1672836908"},
+  {home:"https://eirms.com/",host:"https://memo.eirms.com/",apiV1:'',creatorId:"1",imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=6bLfAytyUI7daRuxat0XxA&kti=ZDt5CwAAAAA&s=140&t=1646284093"},
+  {home:"https://cmsblog.cn/",host:"https://memos.cmsblog.cn/",apiV1:'',creatorId:"101",imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=fcgI8ibPhO3zz3IvgL4bl7Q&kti=ZDuqEgAAAAI&s=140&t=1556624886"},
+  {home:"https://pipuwong.com/",host:"https://memos.pipuwong.com/",apiV1:'v1/',creatorId:"1",imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=5F8TcjFWpLMzia5ibn26vs5A&kti=ZFVJhgAAAAA&s=140&t=1679212034"},
+  {home:"https://ysicing.cloud/",host:"https://note.ysicing.cloud/",apiV1:'v1/',creatorId:"1",imgsrc:cdnGravatar+"5ca16ee587d1e4befd805cf710361f26"},
+  {home:"https://www.kingname.info/",host:"https://memos.kingname.info/",apiV1:'v1/',creatorId:"1",imgsrc:cdnGravatar+"2463f1bd85aea23703f320e67d33f349"},
+  {home:"https://idh.ink/",host:"https://idh.ink/",apiV1:'v1/',creatorId:"1",imgsrc:"https://thirdqq.qlogo.cn/g?b=sdk&k=NAuictuJAIjdPiaHlY6xbDWA&kti=ZHUWoAAAAAA&s=140&t=1683481629"},
+  {home:"https://leonfong.me/",host:"https://memo.leonfong.me/",apiV1:'v1/',creatorId:"1",imgsrc:cdnGravatar+"b147bfa4888f1233882ba5b39ef3bffc"}
 ]
 
 let bbDom = document.querySelector('#bbs');
@@ -63,7 +63,7 @@ function allUrls(){
   //console.log(urls)
   let myHtml = ""
   for(let i=0;i < urls.length;i++){
-    myHtml += `<div class="bbs-urls bbs-url" onclick="urlsNow(this)" data-hostid="${urls[i].host+"u/"+urls[i].creatorId}" data-host="${urls[i].host}" data-apiV1="${urls[i].apiV1}" data-creatorId="${urls[i].creatorId}" data-imgsrc="${urls[i].imgsrc}" data-comment="${urls[i].comment}" data-home="${urls[i].home}" data-twienv="${urls[i].twiEnv}" data-artenv="${urls[i].artEnv}" data-artsite="${urls[i].artSite}" data-endpoint="${urls[i].endpoint}" data-reacttargetid="${urls[i].reacttargetid}" data-availablearraystring="${urls[i].availablearraystring}" data-index="${i}"><img src="${urls[i].imgsrc}" alt=""></div>`
+    myHtml += `<div class="bbs-urls bbs-url" onclick="urlsNow(this)" data-hostid="${urls[i].host+"u/"+urls[i].creatorId}" data-index="${i}"><img src="${urls[i].imgsrc}" alt=""></div>`
   }
   myHtml += '<div class="bbs-urls urls-button" onclick="urlsNow(this)" data-type="random"><svg t="1665928089691" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2562" width="32" height="32"><path d="M913.2 672l98.8 57.1c5.3 3.1 5.3 10.8 0 13.9l-43.4 25L710.4 924c-2.7 1.5-6-0.4-6-3.5V772c0-2.2-1.8-4-4-4H544c-70.4 0-134.4-28.8-180.8-75.2-11.1-11.1-21.2-23.2-30.1-36.1-6.4-9.2-20-9.1-26.4 0.1C260.5 723.9 183.1 768 96 768h-48c-26.5 0-48-21.5-48-48s21.5-48 48-48h48c42.5 0 82.6-16.7 112.9-47.1 30.4-30.4 47.1-70.5 47.1-112.9s-16.7-82.6-47.1-112.9C178.6 368.7 138.4 352 96 352h-48c-26.5 0-48-21.5-48-48s21.5-48 48-48h48c70.4 0 134.4 28.8 180.8 75.2 11.1 11.1 21.2 23.2 30.1 36.1 6.4 9.2 20 9.1 26.4-0.1 46.3-67 123.6-111.1 210.8-111.1H700.4c2.2 0 4-1.8 4-4V103.4c0-3.1 3.3-5 6-3.5l258.2 156 43.4 25.1c5.3 3.1 5.3 10.8 0 13.9L913.2 352 710.4 476c-2.7 1.5-6-0.4-6-3.5V356c0-2.2-1.8-4-4-4H544c-42.5 0-82.6 16.7-112.9 47.1-30.4 30.4-47.1 70.5-47.1 112.9 0 42.5 16.7 82.6 47.1 112.9C461.4 655.3 501.5 672 544 672H700.4c2.2 0 4-1.8 4-4V551.4c0-3.1 3.3-5 6-3.5L913.2 672z" p-id="2563" fill="#f5f5f5"></path></svg></div>'
   myHtml += '<div class="bbs-urls urls-button"><a href="https://immmmm.com/bbs-by-memos/"><svg t="1665929410343" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6308" width="32" height="32"><path d="M906.212134 565.732986 565.732986 565.732986 565.732986 906.212134C565.732986 926.013685 541.666486 959.972 511.97312 959.972 482.297674 959.972 458.213254 926.013685 458.213254 906.212134L458.213254 565.732986 117.734106 565.732986C97.950475 565.732986 63.97424 541.666486 63.97424 511.97312 63.97424 482.279754 97.950475 458.213254 117.734106 458.213254L458.213254 458.213254 458.213254 117.734106C458.213254 97.950475 482.297674 63.97424 511.97312 63.97424 541.666486 63.97424 565.732986 97.950475 565.732986 117.734106L565.732986 458.213254 906.212134 458.213254C925.995765 458.213254 959.972 482.279754 959.972 511.97312 959.972 541.666486 925.995765 565.732986 906.212134 565.732986Z" p-id="6309" fill="#f5f5f5"></path></svg></a></div>'
@@ -82,6 +82,11 @@ function nextFetch(){
 };
 
 function urlsNow(e){
+  let domClass = document.getElementById("bbs-urls")
+  window.scrollTo({
+    top: domClass.offsetTop - 30,
+    behavior: "smooth"
+  });
   let loadHas = document.querySelector("#load") || ''
   if(loadHas) loadHas.remove()
   let domUrls = document.querySelectorAll('#bbs-urls .bbs-urls')
@@ -95,40 +100,26 @@ function urlsNow(e){
     page = 1,offset = 0
     bbDom.innerHTML = loading
     let type = e.getAttribute("data-type")
-    if(type == 'random'){
-      let num = Math.round(Math.random() * (urls.length-1))
-      hostNow = urls[num].host
-      creIdNow = urls[num].creatorId
-      imgsrcNow = urls[num].imgsrc
-      commentNow = urls[num].comment
-      twiEnvNow = urls[num].twiEnv
-      artEnvNow = urls[num].artEnv
-      artSiteNow = urls[num].artSite
-      homeNow = urls[num].home
-      apiV1Now = urls[num].apiV1
-      endpointNow = urls[num].endpoint || ''
-      reacttargetidNow = urls[num].reacttargetid || ''
-      availablearraystringNow = urls[num].availablearraystring || ''
-      domUrls[num].classList.add("url-now")
-    }else{
-      domUrls[e.getAttribute("data-index")].classList.add("url-now")
-      hostNow = e.getAttribute("data-host")
-      creIdNow = e.getAttribute("data-creatorId")
-      imgsrcNow = e.getAttribute("data-imgsrc")
-      commentNow = e.getAttribute("data-comment")
-      twiEnvNow = e.getAttribute("data-twienv")
-      artEnvNow = e.getAttribute("data-artenv")
-      artSiteNow = e.getAttribute("data-artsite")
-      homeNow = e.getAttribute("data-home")
-      apiV1Now = e.getAttribute("data-apiV1")
-      endpointNow = e.getAttribute("data-endpoint")
-      reacttargetidNow = e.getAttribute("data-reacttargetid")
-      availablearraystringNow = e.getAttribute("data-availablearraystring")
-    }
+    let num = (type == 'random') ? Math.round(Math.random() * (urls.length-1)) : e.getAttribute("data-index")
+    hostNow = urls[num].host
+    creIdNow = urls[num].creatorId
+    imgsrcNow = urls[num].imgsrc
+    commentNow = urls[num].comment
+    twiEnvNow = urls[num].twiEnv
+    artEnvNow = urls[num].artEnv
+    artSiteNow = urls[num].artSite
+    homeNow = urls[num].home
+    apiV1Now = urls[num].apiV1
+    endpointNow = urls[num].endpoint || ''
+    reacttargetidNow = urls[num].reacttargetid || ''
+    availablearraystringNow = urls[num].availablearraystring || ''
+    domUrls[num].classList.add("url-now")
     bbUrlNow = hostNow+"api/"+apiV1Now+"memo?creatorId="+creIdNow+"&rowStatus=NORMAL&limit=10"
     fetch(bbUrlNow).then(res => res.json()).then( resdata =>{
       let arrData = resdata || ''
-      if(resdata.data) arrData = resdata.data
+      if(resdata.data){
+        arrData = resdata.data
+      }
       bbDom.innerHTML = ''
       bbsDatas.length = 0
       for(let j=0;j < arrData.length;j++){
@@ -189,7 +180,9 @@ function getNextList(){
   let bbUrl = bbUrlNow+"&offset="+offset;
   fetch(bbUrl).then(res => res.json()).then( resdata =>{
     let arrData = resdata || ''
-    if(resdata.data) arrData = resdata.data
+    if(resdata.data){
+      arrData = resdata.data
+    }
     nextDom = arrData
     nextLength = nextDom.length
     page++
@@ -239,7 +232,9 @@ const fetchBBser = async () => {
         let qsLive = ".bbs-urls.bbs-url[data-hostid='"+url.host+"u/"+url.creatorId+"']"
         document.querySelector(qsLive).classList.add("liveon");
         let arrData = resdata || ''
-        if(resdata.data) arrData = resdata.data
+        if(resdata.data){
+          arrData = resdata.data
+        }
         return arrData
       })
     )
@@ -396,27 +391,34 @@ function updateHTMl(data){
         }
       }
       let memoIdNow = memoUrl.replace(/https\:\/\/(.*\.)?(.*)\..*/,'id-$2-')
-      result += `<li class="${memoIdNow+"memo-"+memoId}"><div class="bbs-avatar"><a href="${data[i].home}" target="_blank" rel="noopener noreferrer"><img src="${data[i].imgsrc}" alt=""></a><a href="${memoUrl}" target="_blank" rel="noopener noreferrer" class="bbs-creator">${data[i].creator}</a><span class="bbs-dot">·</span><span class="bbs-date">${new Date(data[i].updatedTs * 1000).toLocaleString()}</span>`
+      //查找当前 urls 中的 index 位置
+      let uslIndexNow = urls.findIndex(item => item.host === memos);
+      
+      let outSVG = '<span class="bbs-coment-svg bbs-outlink"><svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M864 640a32 32 0 0 1 64 0v224.096A63.936 63.936 0 0 1 864.096 928H159.904A63.936 63.936 0 0 1 96 864.096V159.904C96 124.608 124.64 96 159.904 96H384a32 32 0 0 1 0 64H192.064A31.904 31.904 0 0 0 160 192.064v639.872A31.904 31.904 0 0 0 192.064 864h639.872A31.904 31.904 0 0 0 864 831.936V640zm-485.184 52.48a31.84 31.84 0 0 1-45.12-.128 31.808 31.808 0 0 1-.128-45.12L815.04 166.048l-176.128.736a31.392 31.392 0 0 1-31.584-31.744 32.32 32.32 0 0 1 31.84-32l255.232-1.056a31.36 31.36 0 0 1 31.584 31.584L924.928 388.8a32.32 32.32 0 0 1-32 31.84 31.392 31.392 0 0 1-31.712-31.584l.736-179.392L378.816 692.48z"/></svg></span>'
 
       let comSVG = '<span class="bbs-coment-svg"><svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="20" height="24"><path d="M816 808H672c-4.8 0-8 1.6-11.2 4.8l-80 80c-36.8 36.8-97.6 36.8-136 0l-80-80c-3.2-3.2-6.4-4.8-11.2-4.8h-144c-70.4 0-128-57.6-128-128V232c0-70.4 57.6-128 128-128h608c70.4 0 128 57.6 128 128v448C944 750.4 886.4 808 816 808zm0-64c35.2 0 64-28.8 64-64V232c0-35.2-28.8-64-64-64H208c-35.2 0-64 28.8-64 64v448c0 35.2 28.8 64 64 64h144c20.8 0 41.6 8 56 24l80 80c12.8 12.8 32 12.8 44.8 0l80-80c14.4-14.4 35.2-24 56-24H816zM320 408c27.2 0 48 20.8 48 48v32c0 27.2-20.8 48-48 48s-48-20.8-48-48v-32c0-27.2 20.8-48 48-48zm192 0c27.2 0 48 20.8 48 48v32c0 27.2-20.8 48-48 48s-48-20.8-48-48v-32c0-27.2 20.8-48 48-48zm192 0c27.2 0 48 20.8 48 48v32c0 27.2-20.8 48-48 48s-48-20.8-48-48v-32c0-27.2 20.8-48 48-48z" /></svg></span>'
-
-      let outSVG = '<span class="bbs-coment-svg"><svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="20" height="20"><path d="M864 640a32 32 0 0 1 64 0v224.096A63.936 63.936 0 0 1 864.096 928H159.904A63.936 63.936 0 0 1 96 864.096V159.904C96 124.608 124.64 96 159.904 96H384a32 32 0 0 1 0 64H192.064A31.904 31.904 0 0 0 160 192.064v639.872A31.904 31.904 0 0 0 192.064 864h639.872A31.904 31.904 0 0 0 864 831.936V640zm-485.184 52.48a31.84 31.84 0 0 1-45.12-.128 31.808 31.808 0 0 1-.128-45.12L815.04 166.048l-176.128.736a31.392 31.392 0 0 1-31.584-31.744 32.32 32.32 0 0 1 31.84-32l255.232-1.056a31.36 31.36 0 0 1 31.584 31.584L924.928 388.8a32.32 32.32 0 0 1-32 31.84 31.392 31.392 0 0 1-31.712-31.584l.736-179.392L378.816 692.48z"/></svg></span>'
 
       let emojiReaction = `<emoji-reaction theme="system" class="reaction" endpoint="https://api-emaction.immmmm.com" reacttargetid="${memoIdNow+'memo-'+memoId}"></emoji-reaction>`
       if(endpoint && endpoint != "undefined"){
         emojiReaction = `<emoji-reaction theme="system" class="reaction" endpoint="${endpoint}" reacttargetid="${reacttargetid+memoId}" availablearraystring="${availablearraystring}"></emoji-reaction>`
       }
       
+      let bbContDom = `<div class="bbs-content"><div class="bbs-text">${bbContREG+emojiReaction}</div>`
+      let bbAvaDom = `<div class="bbs-avatar"><a href="${data[i].home}" target="_blank" rel="noopener noreferrer"><img src="${data[i].imgsrc}" alt=""></a><a href="javascript:void(0)" class="bbs-creator" onclick="urlsNow(this)" data-index="${uslIndexNow}">${data[i].creator}</a><span class="bbs-dot">·</span><span class="bbs-date">${new Date(data[i].updatedTs * 1000).toLocaleString()}</span>`
+
+      result += `<li class="${memoIdNow+"memo-"+memoId}">
+                    <a href="${memoUrl}" target="_blank" rel="noopener noreferrer">${outSVG}</a>`
+
       if(comment == '1'){
         if(twiEnv && twiEnv != 'undefined'){
-          result += `<a data-id="${memoId}" data-twienv="${twiEnv}" data-path="${memoUrl}" onclick="loadTwikoo(this)" onmouseenter="insertTwikoo(this)" href="javascript:void(0)" rel="noopener noreferrer">${comSVG}</a></div><div class="bbs-content"><div class="bbs-text">${bbContREG+emojiReaction}</div><div class="item-comment twikoo-${memoId} d-none"><div id="${memoIdNow+'twikoo-'+memoId}"></div></div></div></li>`
+          bbAvaDom += `<a data-id="${memoId}" data-twienv="${twiEnv}" data-path="${memoUrl}" onclick="loadTwikoo(this)" onmouseenter="insertTwikoo(this)" href="javascript:void(0)" rel="noopener noreferrer">${comSVG}</a></div>`
+          result += `${bbAvaDom+bbContDom}<div class="item-comment twikoo-${memoId} d-none"><div id="${memoIdNow+'twikoo-'+memoId}"></div></div></div></li>`
         }else if(artEnv && artEnv != 'undefined'){
-          result += `<a data-id="${memoId}" data-artenv="${artEnv}" data-artsite="${artSite}" data-path="${memoUrl}" onclick="loadArtalk(this)" href="javascript:void(0)" rel="noopener noreferrer">${comSVG}</a></div><div class="bbs-content"><div class="bbs-text">${bbContREG+emojiReaction}</div><div class="item-comment ${memoIdNow+'artalk-'+memoId} d-none"></div></div></li>`
-        }else{
-          result += `<a href="${memoUrl}" target="_blank" rel="noopener noreferrer">${outSVG}</a></div><div class="bbs-content"><div class="bbs-text">${bbContREG+emojiReaction}</div></div></li>`
+          bbAvaDom += `<a data-id="${memoId}" data-artenv="${artEnv}" data-artsite="${artSite}" data-path="${memoUrl}" onclick="loadArtalk(this)" href="javascript:void(0)" rel="noopener noreferrer">${comSVG}</a></div>`
+          result += `${bbAvaDom+bbContDom}<div class="item-comment ${memoIdNow+'artalk-'+memoId} d-none"></div></div></li>`
         }
       }else{
-        result += `</div><div class="bbs-content"><div class="bbs-text">${bbContREG+emojiReaction}</div></div></li>`
+        result += `</div>${bbAvaDom+bbContDom}</div></li>`
       }
 
   }// end for
