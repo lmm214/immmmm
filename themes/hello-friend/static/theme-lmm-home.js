1170280169549e05ddae866f93b904c5ee748fe8
lmm214 <fivemu@gmail.ocm>
2023-08-10 09:58:56 +0800

    f

--- themes/hello-friend/static/theme-lmm-home.js
+++ themes/hello-friend/static/theme-lmm-home.js
@@ -34,7 +34,7 @@ var memoUrl = "https://me.edui.fun/"
 if(albumDom){memoAlbum(6);}
 function memoAlbum(numb){
     let limit = numb || 8;
-    var galleryUrl = memoUrl+"api/v1/memo?rowStatus=NORMAL&limit="+limit+"&tag=相册"
+    var galleryUrl = memoUrl+"api/v1/memo?creatorId=101&rowStatus=NORMAL&limit="+limit+"&tag=相册"
     var localalbumUpdated = JSON.parse(localStorage.getItem("albumUpdated")) || '';
     var localalbumData = JSON.parse(localStorage.getItem("albumData")) || '';
     if(localalbumData){
