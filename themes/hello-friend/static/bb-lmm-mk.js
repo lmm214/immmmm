b8bb333cd99d135c75e6e796634c51aeb5eb071d
lmm214 <fivemu@gmail.ocm>
2023-08-11 00:15:54 +0800

    bb

--- themes/hello-friend/static/bb-lmm-mk.js
+++ themes/hello-friend/static/bb-lmm-mk.js
@@ -261,7 +261,7 @@ function updateHTMl(data){
         <div class="bb-div">
           <div class="bb-tool">
             ${emojiReaction}
-            ${memosOpenIdNow == '' ? '':
+            ${ !memosOpenIdNow ? '':
               `<span class="archive-btn" onclick="archiveMemo(this)" data-id="${bbID}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon-img"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg></span>`
             }
           </div>
@@ -380,7 +380,7 @@ function archiveMemo(e) {
   }
 }
 
-//登录状态下，显示标签列表
+//显示标签列表，需配合 cloudflare worker 食用
 function showTaglist(){
     let bbUrl = 'https://api-memos.immmmm.com/'
     let tagListDom = ""
