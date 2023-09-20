72a4f2d3fcc9979e07c41d5ea967ab470dbb011b
lmm214 <fivemu@gmail.ocm>
2023-09-20 22:20:48 +0800

    bb

--- themes/hello-friend/static/theme-lmm-home.js
+++ themes/hello-friend/static/theme-lmm-home.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 var bbDom = document.querySelector('#bber-talk') || '';
 if(bbDom){memoTalks();}
 function memoTalks(){
-var bbUrl = "https://api-memos-v15.immmmm.com/?url=https://me.edui.fun/api/v1/memo?creatorId=101&rowStatus=NORMAL&limit=10"
+var bbUrl = "https://me.edui.fun/api/v1/memo?creatorId=101&rowStatus=NORMAL&limit=10"
 fetch(bbUrl).then(res => res.json()).then( resdata =>{
     var result = '',resultAll="",data = resdata
     for(var i=0;i < data.length;i++){
@@ -34,7 +34,7 @@ var memoUrl = "https://me.edui.fun/"
 if(albumDom){memoAlbum(6);}
 function memoAlbum(numb){
     let limit = numb || 8;
-    var galleryUrl = "https://api-memos-v15.immmmm.com/?url="+memoUrl+"api/v1/memo?creatorId=101&rowStatus=NORMAL&limit="+limit+"&tag=相册"
+    var galleryUrl = memoUrl+"api/v1/memo?creatorId=101&rowStatus=NORMAL&limit="+limit+"&tag=相册"
     var localalbumUpdated = JSON.parse(localStorage.getItem("albumUpdated")) || '';
     var localalbumData = JSON.parse(localStorage.getItem("albumData")) || '';
     if(localalbumData){
