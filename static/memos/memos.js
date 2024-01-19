e8eed7d628489f147e67417d4730d60bd0ea8c4e
林木木 <fivemu@gmail.com>
2024-01-19 17:24:50 +0800

    Update memos.js

--- static/memos/memos.js
+++ static/memos/memos.js
@@ -101,19 +101,15 @@ var memosEditorCont = `
       </div>
       <div class="memos-editor-footer border-t mt-2 pt-2 ">
         <div class="d-flex">
+	  <div class="editor-selector select outline">
+            <select class="select-memos-value pl-2 pr-4 py-2"><option value="PUBLIC">公开</option><option value="PRIVATE">私有</option></select>
+          </div>
           <div class="button outline random-btn mx-2 p-2">
             <svg xmlns="http://www.w3.org/2000/svg" width=".9rem" height=".9rem" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M2 18h1.4c1.3 0 2.5-.6 3.3-1.7l6.1-8.6c.7-1.1 2-1.7 3.3-1.7H22"/><path d="m18 2l4 4l-4 4M2 6h1.9c1.5 0 2.9.9 3.6 2.2M22 18h-5.9c-1.3 0-2.6-.7-3.3-1.8l-.5-.8"/><path d="m18 14l4 4l-4 4"/></g></svg>
           </div>
           <div class="button outline switchUser-btn d-none d-md-flex mr-2 p-2">
             <svg xmlns="http://www.w3.org/2000/svg" width=".9rem" height=".9rem" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M20 7h-9m3 10H5"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></g></svg>
           </div>
-        </div>
-
-        <div class="editor-selector select outline">
-          <select class="select-memos-value pl-2 pr-4 py-2">
-            <option value="PUBLIC">公开</option>
-            <option value="PRIVATE">私有</option>
-          </select>
         </div>
         <div class="editor-submit d-flex flex-fill justify-content-end">
           <div class="edit-memos d-none">
